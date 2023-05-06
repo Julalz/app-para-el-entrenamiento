ba18840df022a48c75b1d54ab4fc9b07ed41fb01
julianalz <julian942@hotmail.com>
2023-05-07 01:01:31 +0200

    endpoint crear usuario reader, DB workout con su tabla de users, config SMTP mail

---
+++ src/repositories/usersRepository.js
@@ -0,0 +1,34 @@
+const getPool = require("../infrastructure/database");
+
+const createUser = async (user) => {
+  const pool = await getPool();
+  const now = new Date();
+  const sql = `INSERT INTO users(
+    name, email, password, verificationCode, role, createdAt
+    ) VALUES (?, ?, ?, ?, ?, ?)`;
+
+  const { name, email, password, verificationCode } = user;
+  const [created] = await pool.query(sql, [
+    name,
+    email,
+    password,
+    verificationCode,
+    "reader",
+    now,
+  ]);
+
+  return created.insertId;
+};
+
+const findUserByEmail = async (email) => {
+  const pool = await getPool();
+  const sql =
+    "SELECT id, name, email, password, role FROM users WHERE email = ?";
+  const [user] = await pool.query(sql, email);
+  return user[0];
+};
+
+module.exports = {
+  createUser,
+  findUserByEmail,
+};
--- trainningApp.js
+++ trainningApp.js
@@ -4,9 +4,14 @@ require(`dotenv`).config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const userRouter = require("./src/routes/userRouter");
 
 const app = express();
+app.use(express.json());
+
 const { PORT } = process.env;
 const port = PORT | 3000;
+
+app.use("/api/v1/users", userRouter);
 
 app.listen(port, () => console.log(`Running ${PORT}`));
