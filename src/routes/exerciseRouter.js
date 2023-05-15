61e53e3031b472e3a5436ce4dd46852a46dbd918
alvarasdk <alvarokav@gmail.com>
2023-05-15 22:25:34 +0200

    "Arreglos finales"

--- src/routes/exerciseRouter.js
+++ src/routes/exerciseRouter.js
@@ -2,19 +2,19 @@
 const express = require("express");
 const validAdmin = require("../middlewares/validAdmin");
 const createExercise = require("../controllers/workout/createExerciseController");
-const getAllExercise = require("../controllers/workout/getAllExerciseController");
+
 const deleteExerciseById = require("../controllers/workout/deleteExerciseController");
 
 const addLikeWorkout = require("../controllers/workout/addLikeExerciseController");
 const removeLikeWorkout = require("../controllers/workout/removeLikeExerciseController");
 const filterExercises = require("../controllers/workout/filterExerciseBymuscleTypologyController");
 const updateExercise = require("../controllers/workout/updateExerciseController");
+const getAllExercise = require("../controllers/workout/getAllExercisesController");
 const exerciseRouter = express.Router();
 
 exerciseRouter.route("/").all(validAdmin).post(createExercise);
 exerciseRouter.route("/getall").get(getAllExercise);
 exerciseRouter.route("/:id").all(validAdmin).delete(deleteExerciseById);
-exerciseRouter.route("/update").all(validAdmin).patch(updateExercise);
 
 exerciseRouter
   .route("/like/:workoutId")
